b31e8c37b2c38f752d7ddb15590ced8a8013d873
jagatpandya <142373532+jagatpandya@users.noreply.github.com>
2023-12-02 09:24:49 +0530

    Add files via upload

---
+++ rupee.c
@@ -0,0 +1,31 @@
+#include <graphics.h>
+  
+int main() {
+    int gdriver = DETECT, gmode;
+  
+    // initialize the graphics mode and local variables
+    initgraph(&gdriver, &gmode, NULL);
+    
+    setbkcolor(WHITE);
+  
+    for (int i=0; i<454; i++) {
+    	setcolor(BLACK);
+        line(0+i, 125, 186+i, 125);
+    	line(93+i, 125, 93+i, 175);
+	    arc(43+i, 175, 0, 95, 50);
+	    line(0+i, 225, 38+i, 225);
+    	line(0+i, 225, 118+i, 375);
+    	line(0+i, 160, 186+i, 160);
+
+        // hold the program for 20 seconds
+        delay(20);
+  
+        // clear the screen
+        cleardevice();
+    }
+    getch();
+    
+    // close the graphics mode
+    closegraph();
+    return 0;
+}
